ff11dea0f3e17c6ba486653c87eed734883fa4b1
HARISH ADIREDDY <66263857+harishadireddy@users.noreply.github.com>
2021-07-19 18:12:35 +0530

    cross origin

--- next.config.js
+++ next.config.js
@@ -2,5 +2,19 @@ module.exports = {
   env: {
     MONGO_URI: "mongodb+srv://new_user:kXPhbHVEhAeLuUZa@cluster1.rnmer.mongodb.net/notes?retryWrites=true&w=majority",
   },
+     async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   reactStrictMode: true,
 };
