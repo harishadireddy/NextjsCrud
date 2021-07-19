85cf083d1d152443babc68413992f405fa483802
HARISH ADIREDDY <66263857+harishadireddy@users.noreply.github.com>
2021-07-19 18:18:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  env: {
-    MONGO_URI: "mongodb+srv://new_user:kXPhbHVEhAeLuUZa@cluster1.rnmer.mongodb.net/notes?retryWrites=true&w=majority",
-  },
-     async headers() {
+       
+  async headers() {
     return [
       {
         // matching all API routes
@@ -16,5 +14,10 @@ module.exports = {
       }
     ]
   },
+  
+  env: {
+    MONGO_URI: "mongodb+srv://new_user:kXPhbHVEhAeLuUZa@cluster1.rnmer.mongodb.net/notes?retryWrites=true&w=majority",
+  },
+
   reactStrictMode: true,
 };
